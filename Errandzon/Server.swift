943f3d8ad075101a9bf2c9fdeb305a2e20792630
ahkui <ahkui@iMac04.local>
2017-07-10 17:23:40 +0800

    up

---
+++ Errandzon/Server.swift
@@ -0,0 +1,16 @@
+//
+//  Server.swift
+//  Errandzon
+//
+//  Created by ahkui on 2017/7/10.
+//  Copyright © 2017年 Revolutionaries. All rights reserved.
+//
+
+import Foundation
+class Server {
+    
+    
+    init() {
+        
+    }
+}
