19f05c965d1421dcfd5a9167251915d7d0e06e64
ahkui <ahkui@imac04.local>
2017-07-10 19:24:20 +0800

    up

--- Errandzon/FacebookLogInViewController.swift
+++ Errandzon/FacebookLogInViewController.swift
@@ -9,80 +9,69 @@
 import UIKit
 import FBSDKLoginKit
 class FacebookLogInViewController: UIViewController ,FBSDKLoginButtonDelegate{
-        @IBOutlet weak var facebookLogInButton: FBSDKLoginButton!
+    @IBOutlet weak var facebookLogInButton: FBSDKLoginButton!
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            facebookLogInButton.readPermissions = ["public_profile", "email", "user_friends"]
-            facebookLogInButton.delegate = self
-            
-            //第一次登入後可取得使用者token，後續即可直接登入
-            if (FBSDKAccessToken.current()) != nil{
-                fetchProfile()
-            }
-        }
-        
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            FBSDKLoginManager().logOut()
-        }
-        
-        func fetchProfile(){
-            print("fetch profile")
-            
-            let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
-            
-            FBSDKGraphRequest(graphPath: "me", parameters: parameters).start(completionHandler: {
-                connection, result, error -> Void in
-                
-                
-                if error != nil {
-                    print("longinerror =(error)")
-                    return
-                } else {
-                    
-                    if let resultNew = result as? [String:Any]{
-                        
-                        let email = resultNew["email"]  as! String
-                        print(email)
-                        
-                        let firstName = resultNew["first_name"] as! String
-                        print(firstName)
-                        
-                        let lastName = resultNew["last_name"] as! String
-                        print(lastName)
-                        
-                        if let picture = resultNew["picture"] as? NSDictionary,
-                            let data = picture["data"] as? NSDictionary,
-                            let url = data["url"] as? String {
-                            print(url) //臉書大頭貼的url, 再放入imageView內秀出來
-                        }
-                    }
-                    
-                    
-                }
-            })
-        }
-        
-        func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-            
-            print("成功登入")
-            fetchProfile()
-            performSegue(withIdentifier: "setUserNameSegue", sender: loginButton)
-            
-        }
-        
-        func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-            
-        }
-        
-        func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
-            return true
-        }
-        
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        proccessLogin()
+        facebookLogInButton.readPermissions = ["public_profile", "email", "user_friends"]
+        facebookLogInButton.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //            FBSDKLoginManager().logOut()
+    }
+    
+    func fetchProfile(){
+        print("fetch profile")
         
     }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        proccessLogin()
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    func proccessLogin(){
+        if FBSDKAccessToken.current() == nil {
+            return
+        }
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start(completionHandler: {
+            connection, result, error -> Void in
+            if let error = error {
+                print("longinerror =\(error)")
+                return
+            }
+            if let resultNew = result as? [String:Any]{
+                let email = resultNew["email"]  as! String
+                print(email)
+                let firstName = resultNew["first_name"] as! String
+                print(firstName)
+                let lastName = resultNew["last_name"] as! String
+                print(lastName)
+                if let picture = resultNew["picture"] as? NSDictionary,
+                    let data = picture["data"] as? NSDictionary,
+                    let url = data["url"] as? String {
+                    print(url) //臉書大頭貼的url, 再放入imageView內秀出來
+                }
+                self.performSegue(withIdentifier: "setUserNameSegue", sender: nil)
+            }
+            
+        })
+    }
+    
+    
+    
+}
 
 
--- Errandzon/Server.swift
+++ Errandzon/Server.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 class Server {
-    
+    var token:String!
     
     init() {
         
